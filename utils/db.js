3f754662dcb285cb16e8b80e79378dfee1b27206
angga <angga@primaku.com>
2023-05-25 17:12:37 +0700

    modified:   utils/db.js

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,19 @@ const mongoose = require("mongoose");
 
 const mongourl = process.env.MONGOURL || "mongodb://127.0.0.1:27017/dosenjere";
 
-// connect to mongodb
-mongoose.connect(mongourl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// let oMongoose = null;
 
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Database connected"));
+const vConnectMongoose = async () => {
+  // connect to mongodb
+  const oMongoose = await mongoose.connect(mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  const db = oMongoose.connection;
+  db.on("error", (error) => console.error(error));
+  db.once("open", () => console.log("Database connected"));
+};
+
+vConnectMongoose();
